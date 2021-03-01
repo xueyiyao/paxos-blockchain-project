8241221d3125bdcf4eb517027a61dc66a8831a5f
xueyiyao <alleny111@gmail.com>
2021-02-28 22:54:34 -0800

    fix client bugs

--- client.py
+++ client.py
@@ -38,24 +38,25 @@ def handle_inputs():
         except EOFError:
             pass
 
-threading.Thread(target=handle_inputs, args=()).start()
-
-
-while True: 
-    # server listening for msgs
-    global SERVER_SOCKETS
-    try: 
-        word1 = SERVER_SOCKETS[0].recv(1024)
-        word2 = SERVER_SOCKETS[1].recv(1024)
-        word3 = SERVER_SOCKETS[2].recv(1024)
-        word4 = SERVER_SOCKETS[3].recv(1024)
-        word5 = SERVER_SOCKETS[4].recv(1024)
-        print(word1, word2, word3, word4, word5)
-    except KeyboardInterrupt:
-        do_exit(SERVER_SOCKETS[0], SERVER_SOCKETS[1], SERVER_SOCKETS[2], SERVER_SOCKETS[3], SERVER_SOCKETS[4])
+def handle_recv():
+    while True: 
+        # server listening for msgs
+        global SERVER_SOCKETS
+        try: 
+            word1 = SERVER_SOCKETS[0].recv(1024).decode()
+            word2 = SERVER_SOCKETS[1].recv(1024).decode()
+            word3 = SERVER_SOCKETS[2].recv(1024).decode()
+            word4 = SERVER_SOCKETS[3].recv(1024).decode()
+            word5 = SERVER_SOCKETS[4].recv(1024).decode()
+            print(word1, word2, word3, word4, word5)
+        except KeyboardInterrupt:
+            do_exit(SERVER_SOCKETS[0], SERVER_SOCKETS[1], SERVER_SOCKETS[2], SERVER_SOCKETS[3], SERVER_SOCKETS[4])
 
 if __name__ == '__main__':
     for i in range(5):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         SERVER_SOCKETS.append(sock)
-        SERVER_SOCKETS[i].connect(socket.gethostname(), SERVER_PORTS[i])
+        SERVER_SOCKETS[i].connect((socket.gethostname(), SERVER_PORTS[i+1]))
+
+    threading.Thread(target=handle_inputs, args=()).start()
+    handle_recv()
